8bfcb899ab54fc0c387a219a5a6f5015dd5ef020
Amruta Karanjekar <karanjekar.amruta@gmail.com>
2020-06-10 13:50:46 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,64 +1,58 @@
 require('dotenv').config();
-const path = require('path');
-const express = require('express');
-const createError = require('http-errors');
-const logger = require('morgan');
-const bodyParser = require("body-parser");
-const favicon = require('serve-favicon');
 
+const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
 
-// Set up the database
-require('./configs/db.config');
 
-// bind user to view - locals
-const bindUserToViewLocals = require('./configs/user-locals.config');
-
-// Routers
-const indexRouter = require('./routes/index.routes');
-
-const app = express();
-require('./configs/session.config')(app);
-
-// Express View engine setup
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(bindUserToViewLocals);
-app.use(bodyParser.urlencoded({ extended: false }));
+mongoose
+  .connect('mongodb://localhost/daily-manifest-project', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-// Routes middleware
-app.use('/', indexRouter);
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-// Catch missing routes and forward to error handler
-app.use((req, res, next) => next(createError(404)));
 
-// Catch all error handler
-app.use((error, req, res) => {
-  // Set error information, with stack only available in development
-  res.locals.message = error.message;
-  res.locals.error = req.app.get('env') === 'development' ? error : {};
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
 
-//   // render the error page
-  res.status(error.status || 500);
-  res.render('error');
-});
 
-app.listen(process.env.PORT, ()=>{
-  console.log('app is listening');
-})
+
+const index = require('./routes/index');
+app.use('/', index);
+
 
 module.exports = app;
