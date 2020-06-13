75235ec89b6e1e630084c89175b30e4477200b62
Aida <aida.loemba@outlook.com>
2020-06-13 17:06:10 +0200

    reduced overview welcome card and fixed unexpected pink date

--- routes/users/overview.js
+++ routes/users/overview.js
@@ -3,14 +3,19 @@ const app = express();
 const User = require('../../models/user');
 const axios = require('axios').default;
 
-axios({
-  method: "get",
-  url: "/quotes",
-  baseURL: "https://type.fit/api"
-})
-  .then(function (response) {
-    response.data.text()
-  });
+// axios({
+//   method: "get",
+//   url: "/quotes",
+//   baseURL: "https://type.fit/api",
+//   transformResponse: [function (data) {
+//     // Do whatever you want to transform the data
+ 
+//     return data;
+//   }],
+// })
+//   .then(function (response) {
+//     response.data.text()
+//   });
 
 app.get('/users/overview', (req, res) => res.render('users/overview'));
 
