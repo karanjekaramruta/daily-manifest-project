b7ce6d9112663e3955533253950502a9b1c1607c
Aida <aida.loemba@outlook.com>
2020-06-13 16:31:07 +0200

    installed axios

--- routes/users/overview.js
+++ routes/users/overview.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const User = require('../../models/user');
+const axios = require('axios').default;
+
+axios({
+  method: "get",
+  url: "/quotes",
+  baseURL: "https://type.fit/api"
+})
+  .then(function (response) {
+    response.data.text()
+  });
 
 app.get('/users/overview', (req, res) => res.render('users/overview'));
-
 
 module.exports = app;
