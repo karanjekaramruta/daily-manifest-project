ff9339d2d9e00875610cf7329e99e467ed0faed9
varunkumar88 <varunkumarchalotra@gmail.com>
2020-06-15 18:26:39 +0200

    revised version pic upload

--- routes/users/user-Profile.js
+++ routes/users/user-Profile.js
@@ -2,15 +2,25 @@ const express = require("express");
 const app = express();
 const User = require("../../models/user");
 var bodyParser = require("body-parser");
+const multer  = require('multer');
+const upload = multer({ dest: '../../public/images' });
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/users/user-profile/", (req, res) => {
 //console.log(req.session.currentUser._id)deb
-debugger
+
   User.findById(req.session.currentUser._id)
-    .then((user) =>{      
+    
+  
+  .then((user) =>{  
+/*
+    let theDate = new Date(user.dateOfBirth);
+    let YYYMMDD = `${theDate.getDate()}/${theDate.getMonth()+1}/${theDate.getFullYear()}`;
+    user.dateOfBirth = YYYMMDD;*/
+    console.log(user.dateOfBirth);
       res.render("users/user-profile",{upload:user});
     })
     .catch((err)=> {
@@ -19,13 +29,14 @@ debugger
 });
 
 app.post("/users/user-profile", (req,res)=>{
-  debugger
   const {firstName, lastName,  email, dateOfBirth, address, occupation, hobbies} = req.body;
   let user = new User({firstName, lastName,  email, dateOfBirth, address, occupation, hobbies});
   console.log(req.session.currentUser._id);
   User
     .findByIdAndUpdate(req.session.currentUser._id,{firstName, lastName,  email, dateOfBirth, address, occupation, hobbies})
     .then((userData)=>{
+      
+
       res.redirect ('/users/user-profile');
     })
     .catch(err=>{
@@ -34,15 +45,25 @@ app.post("/users/user-profile", (req,res)=>{
 })
 
 
-app.get('/dummy', (req, res, next) => {
-  
-  res.render('dummy');
+/*app.get('/dummy', (req, res, next) => {
+  res.render('dummy'); 
+});*/
+
+app.post('/upload', upload.single('profilePicture'), (req, res, next) => {
+ 
+  User
+  .findByIdAndUpdate(req.session.currentUser._id, {profilePicture: req.file.filename})
+  .then((user) => {
+    res.redirect("/users/user-profile")
+  })
+  .catch((err) => {
+    console.log("Error with uploading profile picture", err)
+  })
+
+})
 
 
 
 
-
-  
-});
 
 module.exports = app;
