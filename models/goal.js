1706a7fd130e69a67dd4821b51f1b1eb87c76e4b
Amruta Karanjekar <karanjekar.amruta@gmail.com>
2020-06-17 21:19:40 +0200

    code refactoring

--- models/goal.js
+++ models/goal.js
@@ -3,43 +3,43 @@ const Schema = mongoose.Schema;
 
 const goalSchema = new Schema({
   title: {
-    type:String,
-    required:[true, 'goal name is mandatory']
+    type: String,
+    required: [true, 'goal name is mandatory']
   },
-  type:{
-    type:String,
-    required:[true, 'goal type is mandatory']
+  type: {
+    type: String,
+    required: [true, 'goal type is mandatory']
   },
-  category:{
-    type:String,
-    required:[true, 'goal category is mandatory']
+  category: {
+    type: String,
+    required: [true, 'goal category is mandatory']
   },
-  startDate:{
-    type:Date,
-    required:[true, 'start date for goal is required']
+  startDate: {
+    type: Date,
+    required: [true, 'start date for goal is required']
   },
-  endDate:{
-    type:Date,
-    required:[true, 'end date for goal is mandatory']
+  endDate: {
+    type: Date,
+    required: [true, 'end date for goal is mandatory']
   },
-  tasks:[{
-    title:{
-      type:String,
-      required:[true, 'title of task is mandatory']
+  tasks: [{
+    title: {
+      type: String,
+      required: [true, 'title of task is mandatory']
     },
-    done:{
-      type:Boolean,
-      default:false
+    done: {
+      type: Boolean,
+      default: false
     }
   }],
-  performedTasks:[{
-    title:{
-      type:String,
-      required:[true, 'title of task is mandatory']
+  performedTasks: [{
+    title: {
+      type: String,
+      required: [true, 'title of task is mandatory']
     },
-    completionDate:{
-      type:Date,
-      required:[true, 'Task completion is mandatory to mark task as completed']
+    completionDate: {
+      type: Date,
+      required: [true, 'Task completion is mandatory to mark task as completed']
     }
   }]
 });
--- models/user.js
+++ models/user.js
@@ -1,38 +1,56 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    firstName: { type: String, default: "" },
-    lastName: { type: String, default: "" },
-    dateOfBirth: { type: Date, default: "" },
-    address: { type: String, default: "" },
-    hobbies: { type: String, default: "" },
-    occupation: { type: String, default: "" },
-    profilePicture: {
-      id: String,
-      path:String,
-      originalFileName:String
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required."],
-      // this match will disqualify all the emails with accidental empty spaces, missing dots in front of (.)com and the ones with no domain at all
-      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required."],
-    },
-    goals: [{ type: mongoose.Schema.Types.ObjectId, ref: "Goal" }],
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    default: ""
   },
-  {
-    timestamps: true,
-  }
-);
+  lastName: {
+    type: String,
+    default: ""
+  },
+  dateOfBirth: {
+    type: Date,
+    default: ""
+  },
+  address: {
+    type: String,
+    default: ""
+  },
+  hobbies: {
+    type: String,
+    default: ""
+  },
+  occupation: {
+    type: String,
+    default: ""
+  },
+  profilePicture: {
+    id: String,
+    path: String,
+    originalFileName: String,
+    default:'/images/default.png'
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+  },
+  goals: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Goal"
+  }],
+}, {
+  timestamps: true,
+});
 
 const User = mongoose.model("User", userSchema);
 
