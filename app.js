1c86f95366079f74b78334d3f579617c796f5258
Aida <aida.loemba@outlook.com>
2020-06-17 20:12:57 +0200

    cleaned code and fixed minor issues

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express      = require('express');
-const app = express();
+const app          = express();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const favicon      = require('serve-favicon');
